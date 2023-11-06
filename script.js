95e61567048789b4abeb7d40482d4fe258891e13
Moon Chu <mmoonchu2@gmail.com>
2023-11-05 21:49:16 -0500

    Added HTML, CSS, implemented message logging function

--- script.js
+++ script.js
@@ -9,19 +9,22 @@
 // b. attack || retreat
 
 let battleStatus; // state var
+let infoLogs = [];
+let infoLogsClone = [];
 
 // Ship constructor
 class ship {
-    constructor(name, hull, firepower, accuracy, alignment){
+    constructor(name, hull, firepower, accuracy){
         this.name = name;
         this.hull = hull;
         this.firepower = firepower;
         this.accuracy = accuracy;
-        this.alignment = alignment;
     }
     attack(ship) {
+        printInfo(`${this.name} takes a shot at ${ship.name}...`);
         if (Math.random() <= this.accuracy) {
             ship.hull -= this.firepower;
+            printInfo(`It hits for ${this.firepower} damage!! ${ship.name} has ${ship.hull} hitpoints remaining.`)
         }
         if (ship.hull <= 0) {
             battleStatus = 0;
@@ -33,29 +36,46 @@ class ship {
 const aliens = [];
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
+}
 const createAlienShips = function(numOfShips) {
     for (let i = 0; i < numOfShips; i++) {
-        aliens[i] = new ship(`Alien ${i + 1}`, getRandomInt(4) + 3, getRandomInt(3) + 2, getRandomInt(3) * .1 + .6, 'Aliens')
+        aliens[i] = new ship(`Alien ${i + 1}`, getRandomInt(4) + 3, getRandomInt(3) + 2, getRandomInt(3) * .1 + .6)
     }
 }
 
+// Function for adding text to infobox
+const printInfo = function(text) {
+    infoLogs.push(text);
+    updateInfobox();
+}
+const updateInfobox = function() {
+    infoLogs.forEach((log, index) => {
+        if (infoLogsClone[index] === undefined) {
+            document.querySelector('#infobox').appendChild(document.createElement('p'));
+            document.querySelector('#infobox').lastChild.classList.add('info');
+            document.querySelector('#infobox').lastChild.innerText = log;
+        }
+    })
+    infoLogsClone = infoLogs.slice();
+}
+
+
 // Initialize ships, begin game
 const runGame = function() {
-    const player = new ship('The USS Assembly', 20, 5, .7, 'Earth');
+    const player = new ship('The USS Assembly', 20, 5, .7);
     createAlienShips(6);
 
     for (const alien of aliens.slice()) {
         doBattle(player, alien)
         // player loses battle (END)
         if (player.hull <= 0) {
-            console.log(`${player.name} is struck down in battle, ultimately allowing the aliens to conquer Earth. Humanity's run ends here.`);
+            printInfo(`${player.name} is struck down in battle, ultimately allowing the aliens to conquer Earth. Humanity's run ends here.`);
             break;
         }
         // player wins battle
         defeatedAlien = aliens.shift();
         if (aliens[0] === undefined) {
-            console.log(`You have defeated all of the aliens. Humanity is saved! 🎉`);
+            printInfo(`You have defeated all of the aliens. Humanity is saved! 🎉`);
             break;
         } else {
             promptContinue();
@@ -71,12 +91,26 @@ const doBattle = function(ship1, ship2) {
     }
 }
 const promptContinue = function() {
-    console.log(`${defeatedAlien.name} defeated! Only ${aliens.length} to go...\nContinue?`);
+    printInfo(`${defeatedAlien.name} defeated! Only ${aliens.length} to go...\n`);
+    document.querySelector('#continue').hidden = !document.querySelector('#continue').hidden;
+    // add button functionality
+    
 }
 
 runGame();
 
 // left to do:
 // finish promptContinue()
-    // html?
+// html?
 // live battle messages
+
+
+// while (input.length !== 1) {
+//     input = prompt(`Continue? (y/n)`);
+// }
+// if (input.toLowerCase() === 'n') {
+//     console.log(`You turn tail and run. Humanity's days remain numbered.`);
+//     this.this.break;
+// }
+
+// printInfo(`${ship.name} goes down!💥 `)
